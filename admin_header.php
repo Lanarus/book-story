622bc1c5fcf8eac8193c603c574c50a68b9d0384
Lanarus <ruslanasabitova0321@gmail.com>
2023-03-15 22:22:15 +0100

    admin header php

--- admin_header.php
+++ admin_header.php
@@ -1,4 +1,15 @@
-
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
 
 <header class="header">
 
@@ -20,8 +31,8 @@
       </div>
 
       <div class="account-box">
-         <p>username : <span></span></p>
-         <p>email : <span></span></p>
+         <p>username : <span><?php echo $_SESSION['admin_name']; ?></span></p>
+         <p>email : <span><?php echo $_SESSION['admin_email']; ?></span></p>
          <a href="logout.php" class="delete-btn">logout</a>
          <div>new <a href="login.php">login</a> | <a href="register.php">register</a></div>
       </div>
