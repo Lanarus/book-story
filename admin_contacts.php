327831e7266b3ce2f35874c93501b6e0ea249686
Lanarus <ruslanasabitova0321@gmail.com>
2023-03-16 21:26:06 +0100

    admin contacts

---
+++ admin_contacts.php
@@ -0,0 +1,53 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>messages</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom admin css file link  -->
+   <link rel="stylesheet" href="css/admin_style.css">
+
+</head>
+<body>
+   
+<?php include 'admin_header.php'; ?>
+
+<section class="messages">
+
+   <h1 class="title"> messages </h1>
+
+   <div class="box-container">
+
+   <div class="box">
+      <p> user id : <span></span> </p>
+      <p> name : <span></span> </p>
+      <p> number : <span></span> </p>
+      <p> email : <span></span> </p>
+      <p> message : <span></span> </p>
+      
+   </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+<script src="js/admin_script.js"></script>
+
+</body>
+</html>
