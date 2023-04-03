a54b144093deee974559948a080c965c914d29df
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-03 13:40:57 +0200

    admin script user btn

--- js/admin_script.js
+++ js/admin_script.js
@@ -5,3 +5,12 @@ document.querySelector('#menu-btn').onclick = () =>{
    navbar.classList.toggle('active');
    accountBox.classList.remove('active');
 }
+document.querySelector('#user-btn').onclick = () =>{
+    accountBox.classList.toggle('active');
+    navbar.classList.remove('active');
+ }
+ 
+ window.onscroll = () =>{
+    navbar.classList.remove('active');
+    accountBox.classList.remove('active');
+ }
