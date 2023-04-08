c07fa50eb1907d1d68ede10d7ab4925d1580a415
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-08 17:04:48 +0200

    home form php

--- home.php
+++ home.php
@@ -67,13 +67,13 @@ if(isset($_POST['add_to_cart'])){
     <div class="box-container">
 
     <form action="" method="post" class="box">
-      <img class="image" src="uploaded_img/" alt="">
-      <div class="name"></div>
-      <div class="price">$/-</div>
+      <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
+      <div class="name"><?php echo $fetch_products['name']; ?></div>
+      <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
       <input type="number" min="1" name="product_quantity" value="1" class="qty">
-      <input type="hidden" name="product_name" value="">
-      <input type="hidden" name="product_price" value="">
-      <input type="hidden" name="product_image" value="">
+      <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+      <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
+      <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
       <input type="submit" value="add to cart" name="add_to_cart" class="btn">
      </form>
 
