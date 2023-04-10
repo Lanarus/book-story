b8470674bd304c0c3737fbfd6cb936e75a145f5f
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-10 11:46:11 +0200

    header

---
+++ header.php
@@ -0,0 +1,46 @@
+
+
+
+<header class="header">
+
+   <div class="header-1">
+      <div class="flex">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <p> new <a href="login.php">login</a> | <a href="register.php">register</a> </p>
+      </div>
+   </div>
+
+   <div class="header-2">
+      <div class="flex">
+         <a href="home.php" class="logo">Bookly.</a>
+
+         <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="shop.php">shop</a>
+            <a href="contact.php">contact</a>
+            <a href="orders.php">orders</a>
+         </nav>
+
+         <div class="icons">
+            <div id="menu-btn" class="fas fa-bars"></div>
+            <a href="search_page.php" class="fas fa-search"></a>
+            <div id="user-btn" class="fas fa-user"></div>
+           
+            <a href="cart.php"> <i class="fas fa-shopping-cart"></i> <span>()</span> </a>
+         </div>
+
+         <div class="user-box">
+            <p>username : <span></span></p>
+            <p>email : <span></span></p>
+            <a href="logout.php" class="delete-btn">logout</a>
+         </div>
+      </div>
+   </div>
+
+</header>
