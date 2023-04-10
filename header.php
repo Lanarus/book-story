94e2d420cfab7343aff7c3a470e9b929bccd4e7e
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-10 12:20:42 +0200

    header php

--- header.php
+++ header.php
@@ -42,13 +42,16 @@ if(isset($message)){
             <div id="menu-btn" class="fas fa-bars"></div>
             <a href="search_page.php" class="fas fa-search"></a>
             <div id="user-btn" class="fas fa-user"></div>
-           
-            <a href="cart.php"> <i class="fas fa-shopping-cart"></i> <span>()</span> </a>
+            <?php
+               $select_cart_number = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+               $cart_rows_number = mysqli_num_rows($select_cart_number); 
+            ?>
+            <a href="cart.php"> <i class="fas fa-shopping-cart"></i> <span>(<?php echo $cart_rows_number; ?>)</span> </a>
          </div>
 
          <div class="user-box">
-            <p>username : <span></span></p>
-            <p>email : <span></span></p>
+            <p>username : <span><?php echo $_SESSION['user_name']; ?></span></p>
+            <p>email : <span><span><?php echo $_SESSION['user_email']; ?></span></p>
             <a href="logout.php" class="delete-btn">logout</a>
          </div>
       </div>
