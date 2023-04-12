0dc05a08966aae3eb1c23e1eea99e79d83029393
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-12 21:22:42 +0200

    search page

--- search_page.php
+++ search_page.php
@@ -10,7 +10,23 @@ if(!isset($user_id)){
    header('location:login.php');
 };
 
+if(isset($_POST['add_to_cart'])){
 
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+    $product_quantity = $_POST['product_quantity'];
+ 
+    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+ 
+    if(mysqli_num_rows($check_cart_numbers) > 0){
+       $message[] = 'already added to cart!';
+    }else{
+       mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
+       $message[] = 'product added to cart!';
+    }
+ 
+ };
 
 ?>
 
@@ -65,6 +81,15 @@ if(!isset($user_id)){
             <input type="hidden" name="product_image" value="<?php echo $fetch_product['image']; ?>">
             <input type="submit" class="btn" value="add to cart" name="add_to_cart">
         </form>
+        <?php
+                    }
+                }else{
+                    echo '<p class="empty">no result found!</p>';
+                }
+            }else{
+                echo '<p class="empty">search something!</p>';
+            }
+        ?>
     </div>
   
 
