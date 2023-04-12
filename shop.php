a474a5b3188c5254c68b0b4882002b15fe65c35f
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-12 21:35:42 +0200

    shop

---
+++ shop.php
@@ -0,0 +1,50 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>shop</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+    <?php include 'header.php'; ?>
+
+    <div class="heading">
+        <h3>our shop</h3>
+        <p> <a href="home.php">home</a> / shop </p>
+    </div>
+
+    <section class="products">
+
+        <h1 class="title">latest products</h1>
+
+        <div class="box-container">
+
+        </div>
+
+    </section>
+
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
