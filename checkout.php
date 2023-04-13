8d44eec601aa73bacbfbc0442053124a4c5cb8f1
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-13 12:05:46 +0200

    checkout form

--- checkout.php
+++ checkout.php
@@ -32,7 +32,57 @@
 
 <section class="checkout">
 
-   
+<form action="" method="post">
+      <h3>place your order</h3>
+      <div class="flex">
+         <div class="inputBox">
+            <span>your name :</span>
+            <input type="text" name="name" required placeholder="enter your name">
+         </div>
+         <div class="inputBox">
+            <span>your number :</span>
+            <input type="number" name="number" required placeholder="enter your number">
+         </div>
+         <div class="inputBox">
+            <span>your email :</span>
+            <input type="email" name="email" required placeholder="enter your email">
+         </div>
+         <div class="inputBox">
+            <span>payment method :</span>
+            <select name="method">
+               <option value="cash on delivery">cash on delivery</option>
+               <option value="credit card">credit card</option>
+               <option value="paypal">paypal</option>
+               <option value="paytm">paytm</option>
+            </select>
+         </div>
+         <div class="inputBox">
+            <span>address line 01 :</span>
+            <input type="number" min="0" name="flat" required placeholder="e.g. flat no.">
+         </div>
+         <div class="inputBox">
+            <span>address line 01 :</span>
+            <input type="text" name="street" required placeholder="e.g. street name">
+         </div>
+         <div class="inputBox">
+            <span>city :</span>
+            <input type="text" name="city" required placeholder="e.g. mumbai">
+         </div>
+         <div class="inputBox">
+            <span>state :</span>
+            <input type="text" name="state" required placeholder="e.g. maharashtra">
+         </div>
+         <div class="inputBox">
+            <span>country :</span>
+            <input type="text" name="country" required placeholder="e.g. india">
+         </div>
+         <div class="inputBox">
+            <span>pin code :</span>
+            <input type="number" min="0" name="pin_code" required placeholder="e.g. 123456">
+         </div>
+      </div>
+      <input type="submit" value="order now" class="btn" name="order_btn">
+   </form>  
 
 </section>
 
