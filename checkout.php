3ede6749786015710571f05b8cb62e1236ea710c
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-13 12:04:58 +0200

    checkout

---
+++ checkout.php
@@ -0,0 +1,53 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>checkout</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="heading">
+   <h3>checkout</h3>
+   <p> <a href="home.php">home</a> / checkout </p>
+</div>
+
+<section class="display-order">
+
+</section>
+
+<section class="checkout">
+
+   
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
