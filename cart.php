e625cd118a16c61d3dacdb0b35868ba972ba01e2
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-13 09:26:10 +0200

    cart

--- cart.php
+++ cart.php
@@ -1,4 +1,34 @@
+<?php
 
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+if(isset($_POST['update_cart'])){
+   $cart_id = $_POST['cart_id'];
+   $cart_quantity = $_POST['cart_quantity'];
+   mysqli_query($conn, "UPDATE `cart` SET quantity = '$cart_quantity' WHERE id = '$cart_id'") or die('query failed');
+   $message[] = 'cart quantity updated!';
+}
+
+if(isset($_GET['delete'])){
+   $delete_id = $_GET['delete'];
+   mysqli_query($conn, "DELETE FROM `cart` WHERE id = '$delete_id'") or die('query failed');
+   header('location:cart.php');
+}
+
+if(isset($_GET['delete_all'])){
+   mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+   header('location:cart.php');
+}
+
+?>
 
 
 <!DOCTYPE html>
@@ -13,7 +43,7 @@
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/style1.css">
 
 </head>
 <body>
